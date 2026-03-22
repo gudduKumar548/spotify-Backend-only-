72b3f24031c82fd6666f2cc53d3b4bcebc32e07c
Guddu Kumar <142442965+gudduKumar548@users.noreply.github.com>
2026-03-22 13:06:27 +0530

    Set default PORT to 3000 if not specified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./src/app');
 const connectDB = require('./src/config/db');
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.listen(PORT, () => {
